45f96b6e77361989a6dd775969d44a21acd4230a
Ömer Karaman <drakken129@gmail.com>
2025-05-18 18:09:12 +0300

    BMU2-34: Added Entities

---
+++ AntivirusProgram.Backend/AntivirusProgram.API/AntivirusProgram.Entities/Models/FileHashRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntivirusProgram.Entities.Models
+{
+    public class FileHashRecord
+    {
+        [Key] public int Id { get; set; }
+
+        [Required][MaxLength(40)] public string Sha256 { get; set; }
+
+        public string FileName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? ScannedAt { get; set; }
+
+        public string ScanResultJson { get; set; }
+    }
+}
