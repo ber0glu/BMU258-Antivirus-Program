baa742e4a6557b6da0ea2edc0cdf5553ef37d71a
Ömer Karaman <drakken129@gmail.com>
2025-06-09 20:39:01 +0300

    Update Cors Policy

--- AntivirusProgram.Backend/AntivirusProgram.API/AntivirusProgram.API/Program.cs
+++ AntivirusProgram.Backend/AntivirusProgram.API/AntivirusProgram.API/Program.cs
@@ -2,35 +2,36 @@ using AntivirusProgram.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controller ve Swagger servisleri ekleniyor
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-builder.Services.AddCors(opt => opt.AddPolicy(name: "MyAllowSpecificOrigins",
-    policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    }
-    ));
-
 builder.Services.AddSwaggerGen();
 
+// CORS politikasý tanýmlanýyor - tüm origin, method ve header izinli
+builder.Services.AddCors(opt => opt.AddPolicy("MyAllowSpecificOrigins", policy =>
+{
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+}));
+
+// Diðer servis konfigürasyonlarý
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureVirusTotalClient();
 
-
 var app = builder.Build();
+
+// Hata yakalama middleware’i
 app.ConfigureExceptionHandler();
+
+// CORS middleware’i Authorization’dan önce çaðrýlýyor
 app.UseCors("MyAllowSpecificOrigins");
 
-app.MapOpenApi();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
+// HTTPS yönlendirme
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
